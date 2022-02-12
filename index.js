dabde9432d337f8b7c65d3e05c271c8266b1de7f
johnallec <clegnn95p30c352m@studenti.unical.it>
2022-02-12 10:26:26 +0100

    post for quotes added, quotesPOTC.js added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const quotesPOTC = require('./my_modules/quotesPOTC');
+
+app.use(express.json());
 
 function GenericObject(id,name){
     this.id=id;
@@ -7,9 +10,14 @@ function GenericObject(id,name){
 }
 
 var courses = [
-    new GenericObject(0,"a"),
-    new GenericObject(1,"b"),
-    new GenericObject(2,"c")
+    new GenericObject(0,"CourseA"),
+    new GenericObject(1,"CourseB"),
+    new GenericObject(2,"CourseC"),
+    new GenericObject(3,"CourseA"),
+    new GenericObject(4,"CourseA"),
+    new GenericObject(5,"CourseC"),
+    new GenericObject(6,"CourseC"),
+    new GenericObject(7,"CourseB")
 ]
 
 app.get("/", (req,res) =>{
@@ -20,12 +28,50 @@ app.get("/api/courses/all", (req,res) =>{
     res.send(courses);
 })
 
+app.get("/api/courses/:courseName", (req,res) => {
+    try{
+        if(courses.find(c => c.name == req.params.courseName)) {
+            let foundCourses = [];
+            for(let i = 0; i < courses.length; ++i)
+                if(courses[i].name == req.params.courseName)
+                    foundCourses.push(courses[i]);
+            res.send(foundCourses);
+        }
+        else res.send("No course found");
+    }
+    catch(err){
+        res.send("Follow this sintax /api/courses/typeAName");
+    }
+})
+
 app.get("/api/courses/", (req,res) => {
     try{
-        res.send(courses[parseInt(req.query.id)].name);
+        res.send(courses[parseInt(req.query.id)]);
     }
     catch(err){
         res.send("Follow this sintax /api/courses?id=typeAnId");
+    }
+})
+
+// QUOTES API
+app.get("/api/quotes/:id", (req,res) => {
+    try{
+        res.send(quotesPOTC.getQuote(parseInt(req.params.id)-1));
+    }
+    catch(err){
+        console.log(err);
+        res.send("Follow this sintax /api/quotes?id=typeAnId");
+    }
+})
+
+app.post("/api/quotes", (req,res) => {
+    try{
+        quotesPOTC.addQuote(req.body.quote);
+        res.send(quotesPOTC.getLastQuote());
+    }
+    catch(err){
+        console.log(err);
+        res.send("Check the documentation!");
     }
 })
 
---
+++ my_modules/quotesPOTC.js
@@ -0,0 +1,33 @@
+var path = require('path');
+const fs = require('fs');
+
+function GenericQuote(id,quote){
+    this.id=id;
+    this.quote=quote;
+}
+
+var quotes = [];
+var quotesPath = path.join(__dirname, '..', 'resources', 'piratesOfTheCaribbeanQuotes.txt');
+var quotesStrings = fs.readFileSync(quotesPath, 'utf8');
+if(quotesStrings!=null)
+    quotesStrings = quotesStrings.split("\n");
+for(let i = 0; i < quotesStrings.length; ++i){
+    quotes.push(new GenericQuote(i,quotesStrings[i]));
+}
+
+function getQuote(id) {
+    return quotes[id];
+}
+
+function getLastQuote() {
+    return quotes[quotes.length-1];
+}
+
+function addQuote(quote) {
+    fs.appendFile(quotesPath,"\n"+quote,'utf-8',function(){});
+    quotes.push(new GenericQuote(quotes.length, quote));
+}
+
+module.exports.getQuote = getQuote;
+module.exports.getLastQuote = getLastQuote;
+module.exports.addQuote = addQuote;
